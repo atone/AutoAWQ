fad7545749942bc5359a20cbac8363c9e4ae77ef
Naitong Yu <ntyu@baai.ac.cn>
2025-01-06 11:00:41 +0800

    删除config中多余的参数

--- examples/padding.py
+++ examples/padding.py
@@ -37,8 +37,11 @@ def pad_model(model_path, output_path):
     print(f'Saving model to {output_path}')
     torch.save(sd, output_path / 'pytorch_model.bin')
     shutil.copy(model_path / 'modeling_teleflm.py', output_path / 'modeling_teleflm.py')
-    # change the intermediate size and save the config
+    # change the intermediate size, remove unneeded attributes, and save the config
     model.config.intermediate_size = 22528
+    # 删除此属性，否则量化时可能报cuda out of memory错误
+    del model.config._attn_implementation_autoset
+    del model.config._name_or_path
     model.config.save_pretrained(output_path)
     # save the tokenizer
     tokenizer.chat_template =   "{%- for message in messages %}" \
